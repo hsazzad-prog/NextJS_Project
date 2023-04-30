d39e7e88d5153a7fb9fe89b4b8c517bf0df084c3
hsazzad-prog <hsazzad02@gmail.com>
2023-04-30 12:38:43 +0600

    deploy vercel v2

--- pages/component/userdata.js
+++ pages/component/userdata.js
@@ -11,7 +11,7 @@ export default function UserLayout({data})
 
 <div className="mx-auto w-72 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
     
-    <div className="flex flex-col items-center pb-10">
+    {/* <div className="flex flex-col items-center pb-10">
               <Image className="w-24 h-24 mb-3 rounded-full shadow-lg"  src={"http:/localhost:3000/admin/getimage/"+data.filename} alt="me" width="150" height="150" />
     
        
@@ -22,7 +22,7 @@ export default function UserLayout({data})
             <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Update</a>
             <a href="#" className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">Delete</a>
         </div>
-    </div>
+    </div> */}
           </div>
    </div>
         </>
